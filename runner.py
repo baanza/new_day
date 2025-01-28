ca58b266f423da3bc77df3271bc067ace2ddb146
baanza <abyaclifford784@gmail.com>
2025-01-28 22:52:35 +0300

    .

--- runner.py
+++ runner.py
@@ -3,4 +3,5 @@ import streamlit as st
 
 st.write("hello world,, there is something running in the background ...⚡")
 
+subprocess.run(['pip', "install", "sqlmodel"])
 subprocess.run(["python", "main.py"])
